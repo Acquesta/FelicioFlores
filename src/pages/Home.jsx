b87feb78d21932215dca0d08485acde99b420eba
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2023-05-08 11:33:00 -0300

    Página Home alterada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,24 @@ function Home() {
         <div>
             <HoteBar />
             <div>
-                <h1>FelicioFLores o melhor das flores</h1>
-                <img className="bg" src="../assets/sergey-shmidt-koy6FlCCy5s-unsplash.jpg" alt="" />
+                <h1>FelicioFlores o melhor das flores</h1>
+                <div className="container">
+                    <div className="image">
+                        <img className="bg" src="/src/assets/sergey-shmidt-koy6FlCCy5s-unsplash.jpg" alt="" />
+                    </div>
+                    <div className="linha">
+                        <div className="coluna">
+                            <h2>Sobre nós</h2>
+                            <p>
+                                Bem-vindo ao nosso site de floricultura online! Aqui, você encontrará uma ampla variedade de flores, arranjos e presentes para todas as ocasiões. Nós nos dedicamos a fornecer aos nossos clientes produtos frescos e de alta qualidade, além de um atendimento excepcional. 
+                            </p>
+                        </div>
+                        <div className="texto2">
+                            <p>Com nosso site, você pode navegar facilmente por nossas opções e fazer seus pedidos com apenas alguns cliques. Quer seja para um aniversário, um casamento ou uma ocasião especial, temos o presente perfeito para você.</p>
+                            <p>Além disso, oferecemos entrega em toda a região, para que você possa surpreender alguém especial com um belo presente, mesmo que esteja longe. Estamos ansiosos para ajudá-lo a tornar seus momentos especiais ainda mais memoráveis com nossas flores frescas e deslumbrantes.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import HoteBar from "./hoteBar";
+import "../assets/login.css"
 
 function Login() {
     return(
@@ -6,7 +7,12 @@ function Login() {
             <HoteBar />
             <h1>Login</h1>
             <div className="card">
-                
+                <div className="inputs">
+                    <h2>Fazer Login</h2>
+                    <input type="text" name="email" id="email" placeholder="Email" />
+                    <input type="password" name="senha" id="senha" placeholder="Senha" />
+                    <input type="button" value="ENTRAR" id="btn" className="btn" />
+                </div>
             </div>
         </div>
     )
